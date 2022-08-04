e3f073a7ede4f083cf9e7f20e488a7775f70c652
statuspartner <vkuchevasov.sp@gmail.com>
2022-08-04 19:00:30 +0300

    Add Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите своё имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") {
+    Console.WriteLine("Ура, это же МАША!");
+}
+else {
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
