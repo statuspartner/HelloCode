65a67970908859bd8181b987dd9811a936349213
admin <vlad@Statuspartner>
2022-08-09 19:11:06 +0300

    Rewrite code to Example012_ArrayLibrary

--- Example012_ArrayLibrary/Program.cs
+++ Example012_ArrayLibrary/Program.cs
@@ -2,7 +2,7 @@
 {
     int length = collection.Length;
     int index = 0;
-    while(index < length);
+    while(index < length)
     {
         collection[index] = new Random().Next(1, 10);
         index++;
@@ -20,7 +20,32 @@ void PrintArray(int[] col)
     }
 }
 
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // сработает, если находимого числа нет в массиве
+
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
 int[] array = new int[10];
 
 FillArray(array);
+array[4] = 4;
+array[6] = 4;
+
 PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
